c84fd46e25415ac57afac5016d31b5a7b1333bcb
Jack <116524824+Jack-OC@users.noreply.github.com>
2024-01-06 15:59:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ var answers = document.querySelectorAll(".answer");
 questions.forEach((question, index) => {
     question.addEventListener("click", function () {
         this.classList.toggle("active");
-        answers[index].classList.toggle("show");
+        answers[index].style.display = answers[index].style.display === 'block' ? 'none' : 'block';
     });
 });
